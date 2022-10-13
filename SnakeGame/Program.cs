d2b3ce1484508ca6fbe1e45cc7deb1bae75747b6
Selim <zouache.selim57@gmail.com>
2022-10-13 21:17:51 +0200

    last commit

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using TDDSnakeGame;
+
+class Program
+{
+
+    private static void Main(string[] args)
+    {
+        StartSnakeGame();
+    }
+
+    private static void StartSnakeGame()
+    {
+        int gameTurn = 50;
+        List<Player> players = new List<Player>();
+        players.Add(new Player("John"));
+        players.Add(new Player("Tom"));
+        Game board = new Game(gameTurn, players);
+        board.StartGame();
+    }
+
+}
